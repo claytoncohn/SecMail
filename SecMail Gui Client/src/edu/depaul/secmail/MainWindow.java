e07af10f7ee414b78064a7ab85eac80031c19ee9
Jacob Burkamper <jacob.burkamper@gmail.com>
2016-10-31 20:12:06 -0500

    Create simple main window. Create simple mail writer window. Rough in cancel buttons.

--- SecMail Gui Client/src/edu/depaul/secmail/MainWindow.java
+++ SecMail Gui Client/src/edu/depaul/secmail/MainWindow.java
@@ -2,10 +2,29 @@ package edu.depaul.secmail;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Button;
+import swing2swt.layout.BoxLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 
 public class MainWindow {
 
-	protected Shell shell;
+	protected Shell shlSecmail;
 
 	/**
 	 * Launch the application.
@@ -26,9 +45,9 @@ public class MainWindow {
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		shlSecmail.open();
+		shlSecmail.layout();
+		while (!shlSecmail.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -39,10 +58,56 @@ public class MainWindow {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("SWT Application");
+		shlSecmail = new Shell();
+		shlSecmail.setSize(466, 378);
+		shlSecmail.setText("SecMail");
+		shlSecmail.setLayout(new FormLayout());
+		
+		Composite composite = new Composite(shlSecmail, SWT.NONE);
+		composite.setLayout(new GridLayout(1, false));
+		FormData fd_composite = new FormData();
+		fd_composite.right = new FormAttachment(0, 298);
+		fd_composite.top = new FormAttachment(0, 10);
+		fd_composite.left = new FormAttachment(0, 155);
+		fd_composite.bottom = new FormAttachment(0, 299);
+		composite.setLayoutData(fd_composite);
+		
+		Button btnNewEmail = new Button(composite, SWT.NONE);
+		btnNewEmail.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				NewMailWindow newMailWindow = new NewMailWindow();
+				newMailWindow.open();
+			}
+		});
+		GridData gd_btnNewEmail = new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1);
+		gd_btnNewEmail.widthHint = 133;
+		btnNewEmail.setLayoutData(gd_btnNewEmail);
+		btnNewEmail.setText("New Email");
+		
+		Button btnFetchMail = new Button(composite, SWT.NONE);
+		GridData gd_btnFetchMail = new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1);
+		gd_btnFetchMail.widthHint = 131;
+		btnFetchMail.setLayoutData(gd_btnFetchMail);
+		btnFetchMail.setText("Fetch Mail");
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		
+		Button btnClose = new Button(composite, SWT.NONE);
+		btnClose.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		GridData gd_btnClose = new GridData(SWT.CENTER, SWT.CENTER, false, false, 1, 1);
+		gd_btnClose.widthHint = 134;
+		btnClose.setLayoutData(gd_btnClose);
+		btnClose.setText("Close");
 
 	}
-
 }
