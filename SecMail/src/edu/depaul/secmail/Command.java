4beba2fa2a235721cd1857ad4f7b038c1e3be8c7
RedDeer <searonix@gmail.com>
2016-11-06 14:22:22 -0600

    some client stuff
    commands and psedocode and some client code

--- SecMail/src/edu/depaul/secmail/Command.java
+++ SecMail/src/edu/depaul/secmail/Command.java
@@ -2,5 +2,5 @@ package edu.depaul.secmail;
 
 //note: enums are automatically serializable in java.
 public enum Command {
-	CLOSE, LOGIN, PASSWORD, EMAIL, ERROR, CONNECT_TEST, CONNECT_SUCCESS
+	CLOSE, GET_NOTIFICATION, SEND_NOTIFICATION, LOGIN, PASSWORD, EMAIL, ERROR, CONNECT_TEST, CONNECT_SUCCESS
 }
