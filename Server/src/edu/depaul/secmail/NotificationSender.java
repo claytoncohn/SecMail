9baccf3322f48cd26e8f2af8171be897bb2ddee6
Jacob Burkamper <jacob.burkamper@gmail.com>
2016-11-13 14:16:18 -0600

    Fixes for NotificationSender

--- Server/src/edu/depaul/secmail/NotificationSender.java
+++ Server/src/edu/depaul/secmail/NotificationSender.java
@@ -1,6 +1,7 @@
 package edu.depaul.secmail;
 //David Keller
 
+import java.io.IOException;
 import java.net.Socket;
 import java.util.LinkedList;
 
@@ -14,24 +15,29 @@ public class NotificationSender implements Runnable{
 
 	public void run(){
         for (Notification n: this.notifications){
-            if (secMailServer.getGlobalconfig().getDomain() == n.getTo().getDomain()){
-                SecMailServer.addNotificationToList()
+            if (SecMailServer.getGlobalConfig().getDomain() == n.getTo().getDomain()){
+                SecMailServer.addNotificationToList(n);
             }
             else
             {
-                Socket remote = new Socket(n.getTo().getDomain(), n.getTo().getPort());
-                DHEncryptionIO io = new DHEncryptionIO(remote);
-                PacketHeader notificationHeader = new PacketHeader(Command.SEND_NOTIFICATION)
-
-                //Send NotificationHeader over Network
-                io.writeObject(notificationHeader);
-
-                //Send the Notification
-                io.writeObject(n);
-
-                writeObject(new PacketHeader(Command.CLOSE));
-                io.close();
-                remote.close();
+                try {
+	            	Socket remote = new Socket(n.getTo().getDomain(), n.getTo().getPort());
+	                DHEncryptionIO io = new DHEncryptionIO(remote, false);
+	                PacketHeader notificationHeader = new PacketHeader(Command.SEND_NOTIFICATION);
+	
+	                //Send NotificationHeader over Network
+	                io.writeObject(notificationHeader);
+	
+	                //Send the Notification
+	                io.writeObject(n);
+	
+	                io.writeObject(new PacketHeader(Command.CLOSE));
+	                io.close();
+	                remote.close();
+                } catch (IOException e) {
+                	Log.Error("Exception trying to send notificatio to user: "+n.getTo().compile());
+                	Log.Error(e.toString());
+                }
             }
         }
     }
