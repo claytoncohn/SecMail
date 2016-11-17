c5ef280b1443e437fd0e87eb4bc70abf0070e99b
Jacob Burkamper <jacob.burkamper@gmail.com>
2016-11-17 17:02:55 -0600

    Untested code to write notifications to disk and load from disk on the server

--- Server/src/edu/depaul/secmail/SecMailServer.java
+++ Server/src/edu/depaul/secmail/SecMailServer.java
@@ -2,11 +2,17 @@ package edu.depaul.secmail;
 
 import java.net.*;
 import java.util.LinkedList;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class SecMailServer {
 	private static Config serverConfig;
 	public static LinkedList<Notification> notifications;
+	private static ObjectOutputStream notificationWriter = null;
 
 	public static void main(String[] args) {
 		// read the command line arguments and set up the configuration
@@ -38,12 +44,50 @@ public class SecMailServer {
 
 	}
 	
-	public static void loadNotifications()
+	//Jacob Burkamper
+	private static void loadNotifications()
 	{
-		//TODO:
-		//Implement code to read the notifications from last shutdown here.
-		//for now, just instantiate a blank list
-		notifications = new LinkedList<Notification>();
+		notifications = new LinkedList<Notification>(); // instantiate empty list
+		File notificationFile = new File("Notifications.bin"); // TODO: make this path part of Config
+		if (notificationFile.exists()) // check for the notifications file
+		{
+			try {
+				ObjectInputStream inStream = new ObjectInputStream(new FileInputStream(notificationFile));
+				Notification n = null;
+				try {
+					//load any notifications in the file into the list
+					while ((n = (Notification)inStream.readObject()) != null)
+						notifications.add(n);
+				} catch (ClassNotFoundException e) {
+					//errors in case notifications don't load.
+					Log.Error("ClassNotFound Exception while reading notifications file");
+					Log.Error("Were the notifications saved by a different version of the server?");
+					Log.Error("Error was: " + e.toString());
+				}
+			} catch (IOException e) {
+				//generic IO errors.
+				Log.Error("IO Error while reading notifications file");
+				Log.Error(e.toString());
+			}
+			
+		}
+		//regardless of whether the file exists or not, open a writer.
+		//this will be used to save notifications later.
+		try {
+			notificationWriter = new ObjectOutputStream(new FileOutputStream(notificationFile));
+		} catch (IOException e) {
+			Log.Error("IO Exception while trying to create notificationWriter");
+			Log.Error(e.toString());
+		}
+		
+	}
+	
+	//save the notifications list to a file
+	//TODO: make the location of the notifications file part of Config.
+	//Jacob Burkamper
+	private static void saveNotification(Notification n) 
+	{
+		
 	}
 	
 	public static synchronized LinkedList<Notification> getNotificationList(String username)
@@ -71,7 +115,7 @@ public class SecMailServer {
 		//don't forget to synchronize! (thread safety!)
 		
 		notifications.add(n);
-		
+		saveNotification(n); // save the notification to disk
 		return;
 	}
 	
